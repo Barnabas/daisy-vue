3575623bd96d10ebee25bb5c5cf0e9cb12a21922
Barnabas Kendall <barnabas.kendall@gmail.com>
2025-12-22 14:34:56 -0500

    fix source links

--- src/layouts/LayoutDemo.vue
+++ src/layouts/LayoutDemo.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
-import { COMPONENT_LIST } from '@/utils/components'
+import { COMPONENT_LIST, SOURCE_LINK_ROOT } from '@/utils/components'
 import { computed } from 'vue'
 
 const props = defineProps<{ id: string }>()
 
 const componentItem = computed(() => COMPONENT_LIST.find((item) => item.id == props.id))
 
-const daisyUILink = computed(() => 'https://daisyui.com' + componentItem.value?.path + '/')
+const daisyUILink = computed(() => `https://daisyui.com${componentItem.value?.path}/`)
+
+const componentSourceLink = computed(() => `${SOURCE_LINK_ROOT}${componentItem.value?.sourcePath}`)
+
+const demoSourceLink = computed(() => `${SOURCE_LINK_ROOT}${componentItem.value?.demoPath}`)
 </script>
 <template>
   <section v-if="componentItem">
     <div class="flex gap-4 items-baseline">
       <h1 class="grow">{{ componentItem.label }}</h1>
-      <a class="link" :href="componentItem.sourcePath">Component Source</a>
-      <a class="link" :href="daisyUILink">daisyUI Reference</a>
+      <a class="link" :href="componentSourceLink">Component</a>
+      <a class="link" :href="demoSourceLink">Demo</a>
+      <a class="link" :href="daisyUILink">daisyUI</a>
     </div>
     <slot />
   </section>
