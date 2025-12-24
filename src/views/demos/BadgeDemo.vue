c37d2117f60c85bda4cdf4733a96e9531071106d
Barnabas Kendall <barnabas.kendall@gmail.com>
2025-12-24 09:04:22 -0500

    update dependencies, format

--- src/views/demos/BadgeDemo.vue
+++ src/views/demos/BadgeDemo.vue
@@ -41,9 +41,7 @@ const BADGE_SIZES: BadgeSize[] = ['xs', 'sm', 'md', 'lg', 'xl']
     </div>
     <div class="my-2 flex flex-wrap gap-2 items-baseline">
       <h3 class="w-24">In text:</h3>
-      <p>
-        This is a paragraph with a <BaseBadge color="primary">badge</BaseBadge> inside it.
-      </p>
+      <p>This is a paragraph with a <BaseBadge color="primary">badge</BaseBadge> inside it.</p>
     </div>
   </LayoutDemo>
 </template>
